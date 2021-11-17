2dbc378da40f3cf2e37cb838612ffd4111380246
Liedson13 <lvaz2021@elam.co.uk>
2021-11-17 16:59:50 +0000

    add

--- 3D Project/Assets/CharacterController.cs
+++ 3D Project/Assets/CharacterController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-  public float maxspeed = 1.0f;
+    public float maxspeed = 1.0f;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
     float rotation = 0.0f;
     float camRotation = 0.0f;
     GameObject cam;
@@ -13,9 +16,13 @@ public class CharacterController : MonoBehaviour
     bool IsOnGround;
     public GameObject groundChecker;
     public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
+
+    public float maxSprint = 5.0f;
+    float sprintTimer;
 
     void Start()
     {
@@ -31,7 +38,24 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        IsOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (IsOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+       
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxspeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        }else
+        {
+            maxspeed = normalSpeed;
+            sprintTimer = sprintTimer + Time.deltaTime;
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
 
         Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxspeed) + (transform.right * Input.GetAxis("Horizontal") * maxspeed);
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
